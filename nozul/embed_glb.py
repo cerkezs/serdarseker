b5a6180d23934b8644fe7a58a4fbbc2083d0f9c5
cerkezs <134074146+cerkezs@users.noreply.github.com>
2026-04-27 18:26:15 +0300

    Delete nozul directory

--- nozul/embed_glb.py
+++
@@ -1,8 +0,0 @@
-import base64
-with open('karebicak.glb', 'rb') as f:
-    b64 = base64.b64encode(f.read()).decode('utf-8')
-with open('demo-3d-card.html', 'r', encoding='utf-8') as f:
-    html = f.read()
-html = html.replace('src="./karebicak.glb"', f'src="data:model/gltf-binary;base64,{b64}"')
-with open('demo-3d-card.html', 'w', encoding='utf-8') as f:
-    f.write(html)
