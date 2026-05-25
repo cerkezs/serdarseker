f81406392fb2b22063cc851fddf095fe82271d7f
cerkezs <134074146+cerkezs@users.noreply.github.com>
2026-05-25 15:50:57 +0300

    Add files via upload

---
+++ yurdunal-teklif/sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'yurdunal-teklif-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './html2pdf.bundle.min.js',
+  './logo.jpeg',
+  './kase.png',
+  './simza.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response; // Önbellekten döndür
+        }
+        return fetch(event.request); // İnternetten al
+      })
+  );
+});
